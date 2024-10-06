9670f145aaa3f7c9a1fc619d8675b79141cf74e9
Charlie Hayes <77940502+chayesss@users.noreply.github.com>
2024-10-05 23:12:43 -0400

    Connect Firestore to project  (#10)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collectionData, collection, addDoc, CollectionReference } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { item } from './shared/models/item.model';
+import { CommonModule } from '@angular/common';
+import { namedQuery } from 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'knowledge-quest';
+export class AppComponent implements OnInit {
+  // DEMONSTRATION ONLY - FIRESTORE LOGIC SHOULD BE MOVED TO A SERVICE LAYER
+  title: string = 'knowledge-quest';
+  items: item[] = [];
+  test: string = 'testing'
+
+  constructor(
+    private firestore: Firestore,
+  ) {
+  }
+
+  ngOnInit(): void {
+    var itemCollection = collection(this.firestore, 'items');
+    console.log(itemCollection)
+    var data = collectionData<item>(itemCollection, { name: 'test' });
+    data.subscribe((data: item[]) => this.items = data)
+  }
+
+  async onCreateItem(name: string) {
+
+    var item: item = {
+      name: name,
+      createdOn: Date.now().toString(),
+    }
+
+    var docRef = await addDoc(collection(this.firestore, 'items'), item);
+
+    console.log('Doc written with ID: ', docRef.id)
+  }
+
+
 }
+
