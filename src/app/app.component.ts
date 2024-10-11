d8efc0e2f1edc8d8a000acc9c34016cc500eaafe
Charlie Hayes <charlie@balancepoint.com>
2024-10-11 16:25:47 -0400

    move existing code to new dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Firestore, collectionData, collection, addDoc, CollectionReference } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { item } from './shared/models/item.model';
 import { CommonModule } from '@angular/common';
-import { namedQuery } from 'firebase/firestore';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbar } from '@angular/material/toolbar'
 import { MatIcon } from '@angular/material/icon'
@@ -19,35 +14,8 @@ import { MatIcon } from '@angular/material/icon'
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
-  // DEMONSTRATION ONLY - FIRESTORE LOGIC SHOULD BE MOVED TO A SERVICE LAYER
-  title: string = 'knowledge-quest';
-  items: item[] = [];
-  test: string = 'testing'
+export class AppComponent {
 
-  constructor(
-    private firestore: Firestore,
-  ) {
-  }
-
-  ngOnInit(): void {
-    var itemCollection = collection(this.firestore, 'items');
-    console.log(itemCollection)
-    var data = collectionData<item>(itemCollection, { name: 'test' });
-    data.subscribe((data: item[]) => this.items = data)
-  }
-
-  async onCreateItem(name: string) {
-
-    var item: item = {
-      name: name,
-      createdOn: Date.now().toString(),
-    }
-
-    var docRef = await addDoc(collection(this.firestore, 'items'), item);
-
-    console.log('Doc written with ID: ', docRef.id)
-  }
 
 
 }
